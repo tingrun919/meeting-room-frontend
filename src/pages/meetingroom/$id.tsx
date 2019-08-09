d9d7e32b1406a1fbb5dc730cdcb409c9082e57ca
wheeljs <fang.tian@85ido.com>
2019-08-09 17:50:08 +0800

    feat(meetingroom/item): 把添加预约组件移动到components 中，避免创建路由

--- src/pages/meetingroom/$id.tsx
+++ src/pages/meetingroom/$id.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
-import { Accordion } from 'antd-mobile';
+import { Accordion, WhiteSpace } from 'antd-mobile';
 
-import AddReservation from './AddReservation';
+import AddReservation from './components/AddReservation';
+import ReservationList from './components/ReservationList';
 
 interface MeetingRoomItemProps extends RouteComponentProps<{ id: string }> {
 
 }
 
 export default function MeetingRoomItem({ match }: MeetingRoomItemProps) {
+    const { id: roomId } = match.params;
     return (
       <div>
         <Accordion>
@@ -16,6 +18,8 @@ export default function MeetingRoomItem({ match }: MeetingRoomItemProps) {
             <AddReservation />
           </Accordion.Panel>
         </Accordion>
+        <WhiteSpace size="lg" />
+        <ReservationList roomId={roomId}/>
       </div>
     );
 }
