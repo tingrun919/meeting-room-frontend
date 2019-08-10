77a2264bbd0438bab102a3510b56c38233e001cd
wheeljs <fang.tian@85ido.com>
2019-08-11 01:06:38 +0800

    feat(common): 添加不成熟的转场动画

--- src/global.ts
+++ src/global.ts
@@ -3,6 +3,8 @@ import {
   faLock
 } from '@fortawesome/free-solid-svg-icons';
 
+import 'animate.css/animate.css';
+
 library.add(
   faLock
 );
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import withRouter from 'umi/withRouter';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
 import { WingBlank } from 'antd-mobile';
 
 const BasicLayout: React.FC = props => {
+  const { location } = props;
   return (
     <WingBlank size="md">
-      {props.children}
+      <SwitchTransition>
+        <CSSTransition
+          timeout={270}
+          key={location.pathname}
+          classNames={{
+            enter: 'slideInRight',
+            enterActive: 'slideInRight animated',
+            enterDone: 'slideInRight animated',
+            exit: 'slideOutLeft',
+            exitActive: 'slideOutLeft animated',
+            exitDone: 'slideOutLeft animated',
+          }}
+        >
+          {props.children}
+        </CSSTransition>
+      </SwitchTransition>
     </WingBlank>
   );
 };
 
-export default BasicLayout;
+export default withRouter(BasicLayout);
