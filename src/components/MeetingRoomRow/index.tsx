7d37343799201c55df22c4cbecb4cc138a1be7d8
tingrun919 <344197917@qq.com>
2019-08-12 17:46:09 +0800

    feat(meetingRoomRow): 抽离预约组件
    列表预约组件抽离
    
    Close: #22

--- src/components/MeetingRoomRow/index.tsx
+++ src/components/MeetingRoomRow/index.tsx
@@ -1,57 +1,22 @@
 
 import React from 'react';
-import { Tag, Card } from 'antd-mobile';
-import dayjs from '@/utils/dayjs';
-import styles from './index.less';
+import { Card } from 'antd-mobile';
 
 import MeetingRoomDetail from '../MeetingRoomDetail'
+import MeetingRoomReservation from './components/MeetingRoomReservation';
 
 const MeetingRoomRow = ({ rowData, rowID }) => {
-	let current = {
-		creator: '暂无'
-	};
-	let next = {
-		creator: '暂无'
-	};
-	//当前预约
-	if (rowData.currentReservation) {
-		current.creator = `${rowData.currentReservation.creator}`
-		current.time = `${dayjs(rowData.currentReservation.startTime).format('YY-MM-DD HH:mm')}` + '至' + `${dayjs(rowData.currentReservation.endTime).format('YY-MM-DD HH:mm')}`
-	}
-	//下个预约
-	if (rowData.nextReservation) {
-		next.creator = `${rowData.nextReservation.creator}`
-		next.time = `${dayjs(rowData.nextReservation.startTime).format('YY-MM-DD HH:mm')}` + '至' + `${dayjs(rowData.nextReservation.endTime).format('YY-MM-DD HH:mm')}`
-	}
 	return (
 		<Card
 			key={rowID}
 		>
 			<Card.Body>
-				{/* meeting-room description jsx start */}
 				<MeetingRoomDetail
 					meetingRoom={rowData}
 				/>
-				{/* meeting-room descrption jsx end */}
-				{/* reservation jsx start */}
-				<div className={`${styles.marginTop10} ${styles.roomDescrption}`}>
-					<div className={styles.roomDescrption}>
-						<span>当前预约：{current.creator}</span>
-						{current.time ?
-							<span>时间：{current.time}</span>
-							: null
-						}
-					</div>
-					<div className={styles.lineDashedGray}></div>
-					<div className={`${styles.fontWeight300} ${styles.roomDescrption}`}>
-						<span>下个预约：{next.creator}</span>
-						{next.time ?
-							<span>时间：{next.time}</span>
-							: null
-						}
-					</div>
-				</div>
-				{/* reservation jsx end */}
+				<MeetingRoomReservation
+					reservationData={rowData}
+				/>
 			</Card.Body>
 		</Card >
 	);
