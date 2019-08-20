2981f1afb877e43f7b5b590ef9e6d133b9ceef87
wheeljs <fang.tian@85ido.com>
2019-08-20 18:04:14 +0800

    feat(user): 当应用在钉钉环境中初始化时获取用户信息

--- src/global.ts
+++ src/global.ts
@@ -3,8 +3,28 @@ import {
   faLock
 } from '@fortawesome/free-solid-svg-icons';
 
+import dd from 'dingtalk-jsapi';
+
 import 'animate.css/animate.css';
 
 library.add(
   faLock
 );
+
+if (dd.env.platform !== 'notInDingTalk') {
+  dd.ready(async () => {
+    try {
+      const result = await dd.runtime.permission.requestAuthCode({
+        corpId: 'ding57647d02b92db630'
+      });
+      (window as any).g_app._store.dispatch({
+        type: 'user/fetchUser',
+        payload: {
+          code: result.code
+        }
+      });
+    } catch(err) {
+      alert('获取钉钉授权失败：\n' + JSON.stringify(err));
+    };
+  });
+}
